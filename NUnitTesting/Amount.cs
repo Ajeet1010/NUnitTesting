7facfd0272235ccc673004a10c5dcfd55817303b
ajeet1010 <ajeetpal101999@gmail.com>
2023-02-18 18:09:20 +0530

    [Ajeet] Calculating the amount paid monthly taken for loan

---
+++ NUnitTesting/Amount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    internal class Amount
+    {
+
+        public static void MonthlyPayment()
+        {
+            Console.Write("\nEnter the princple amount that has been taken: ");
+            int P = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the no of years for which amount has been taken: ");
+            int Y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the rate of interest at which amount has been given:  ");
+            float R = float.Parse(Console.ReadLine());
+            float r = R / (12 * 100);
+            int n = 12 * Y;
+            double payment = (P * r) / (1 - (Math.Pow((1 + r), -n)));
+            Console.WriteLine("\nMonthly payment for {0} amount that has been taken on a interest of {1} for {2} years is :  {3}", P, R, Y, payment);
+
+        }
+    }
+}
--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -5,9 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("-----  NUnit Testing  -----");
-            Console.WriteLine("Conversion of temperature  in Celsius or Fahrenheit");
-            Temperature temperature = new Temperature();
-            temperature.Conversion();
+            Console.WriteLine("Monthly payment on a amount taken for loan");
+            Amount.MonthlyPayment();
         }
     }
 }
