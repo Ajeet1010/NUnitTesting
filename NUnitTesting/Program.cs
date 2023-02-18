f099822f4b53bea072972b3d2e28df7c84a5d231
ajeet1010 <ajeetpal101999@gmail.com>
2023-02-18 17:32:23 +0530

    [Ajeet] Calculating week day using Georgian Calender

--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("-----  NUnit Testing  -----");
-            Console.WriteLine("$ to be returned by vending machine: ");
-            VendingMachine vendingMachine = new VendingMachine();
-            vendingMachine.Currency();
+            Console.WriteLine("Using Georgian Calender finding weekday on the given date ");
+            WeekDays weekDays = new WeekDays();
+            WeekDays.Calender();
         }
     }
 }
